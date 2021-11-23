a74e18f1d4fa9b2e4e0f8d38f33f845e15caf4b4
Dennis Sheets <dennissheets@outlook.com>
2021-11-23 11:04:43 -0600

    commit 1

---
+++ Ch4-studio-restaurantMenu/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch4_studio_restaurantMenu
+{
+    internal class Item
+    {
+  
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Boolean IsNew { get; set; }
+
+        public Item(string name, string description, double price, string category)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Category = category;
+            IsNew = false;
+            DateCreated = DateTime.Now; 
+        }
+
+
+    }
+}
---
+++ Ch4-studio-restaurantMenu/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch4_studio_restaurantMenu
+{
+    internal class Menu
+    {
+        private List<Item> items = new List<Item>();
+        private static List<string> categories = new List<string> { "appetizer","main course","dessert"};
+        private DateTime lastUpdateTime { get; set; }
+        private string MenuTitle { get; set; }
+
+        public Menu(string menuTitle)
+        {
+            this.items = new List<Item>();
+            this.lastUpdateTime = DateTime.Now;
+            MenuTitle = menuTitle;
+        }
+
+        public void addItem(Item item)
+        {
+            if (!items.Contains(item))
+            {
+                items.Add(item);
+            }
+        }
+
+        public void deleteItem(Item item)
+        {
+            if (items.Contains(item))
+            {
+                items.Remove(item);
+            }
+        }
+
+        public void printMenu()
+        {
+            Console.WriteLine(MenuTitle);
+            Console.WriteLine($"Last updated on: {lastUpdateTime}\n");
+            foreach (string category in categories)
+            {
+                Console.WriteLine($"\t{category}\n");
+                foreach (Item item in items)
+                {
+                    if(item.Category == category)
+                    {
+                        Console.WriteLine($"\t\t{item.Name}\t{item.Description}\t\t${item.Price}");
+                    }
+                      
+                }
+                Console.WriteLine("\t");
+            }
+        }
+    }
+}
